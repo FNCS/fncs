029e91bdd7cfa454b5785613f45382cd1dae7e84
Jeff Daily <jeff.daily@pnnl.gov>
2015-01-28 11:09:53 -0800

    add easylogging++.h

--- tests/test.cpp
+++ tests/test.cpp
@@ -1,9 +1,18 @@
 #include "config.h"
 
+#include "easylogging++.h"
+_INITIALIZE_EASYLOGGINGPP
+using namespace easyloggingpp;
+
 #include "fncs.hpp"
 
 int main(int argc, char **argv)
 {
+    Loggers::setFilename("test.log");
+    Loggers::reconfigureAllLoggers(ConfigurationType::ToStandardOutput, "false");
+    LINFO << "This is my first info";
+    LTRACE << "This is my first trace";
+
     return 0;
 }
 
