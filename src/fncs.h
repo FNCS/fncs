cfcf1fe16f720f5dba953ea8691ae292ea510691
Jeff Daily <jeff.daily@pnnl.gov>
2015-08-27 09:06:33 -0700

    change windows export process

--- src/fncs.h
+++ src/fncs.h
@@ -4,23 +4,16 @@
 #ifndef _FNCS_H_
 #define _FNCS_H_
  
-#if defined _WIN32 && !defined __GNUC__
-# ifdef LIBFNCS_BUILD
-#  ifdef DLL_EXPORT
-#   define LIBFNCS_SCOPE            __declspec (dllexport)
-#   define LIBFNCS_SCOPE_VAR extern __declspec (dllexport)
-#  endif
-# elif defined _MSC_VER
-#  define LIBFNCS_SCOPE
-#  define LIBFNCS_SCOPE_VAR  extern __declspec (dllimport)
-# elif defined DLL_EXPORT
-#  define LIBFNCS_SCOPE             __declspec (dllimport)
-#  define LIBFNCS_SCOPE_VAR  extern __declspec (dllimport)
-# endif
-#endif
-#ifndef LIBFNCS_SCOPE
-# define LIBFNCS_SCOPE
-# define LIBFNCS_SCOPE_VAR extern
+#if defined (__WINDOWS__)
+#   if defined LIBFNCS_STATIC
+#       define FNCS_EXPORT
+#   elif defined LIBFNCS_EXPORTS
+#       define FNCS_EXPORT __declspec(dllexport)
+#   else
+#       define FNCS_EXPORT __declspec(dllimport)
+#   endif
+#else
+#   define FNCS_EXPORT
 #endif
 
 #ifdef __cplusplus
@@ -30,65 +23,65 @@ extern "C" {
     typedef unsigned long long fncs_time;
 
     /** Connect to broker and parse config file. */
-    LIBFNCS_SCOPE void fncs_initialize();
+    FNCS_EXPORT void fncs_initialize();
 
     /** Connect to broker and parse inline configuration. */
-    LIBFNCS_SCOPE void fncs_initialize_config(const char *configuration);
+    FNCS_EXPORT void fncs_initialize_config(const char *configuration);
 
     /** Request the next time step to process. */
-    LIBFNCS_SCOPE fncs_time fncs_time_request(fncs_time next);
+    FNCS_EXPORT fncs_time fncs_time_request(fncs_time next);
 
     /** Publish value using the given key. */
-    LIBFNCS_SCOPE void fncs_publish(const char *key, const char *value);
+    FNCS_EXPORT void fncs_publish(const char *key, const char *value);
 
     /** Publish value using the given key, adding from:to into the key. */
-    LIBFNCS_SCOPE void fncs_route(
+    FNCS_EXPORT void fncs_route(
             const char *from,
             const char *to,
             const char *key,
             const char *value);
 
     /** Tell broker of a fatal client error. */
-    LIBFNCS_SCOPE void fncs_die();
+    FNCS_EXPORT void fncs_die();
 
     /** Close the connection to the broker. */
-    LIBFNCS_SCOPE void fncs_finalize();
+    FNCS_EXPORT void fncs_finalize();
 
     /** Update minimum time delta after connection to broker is made.
      * Assumes time unit is not changing. */
-    LIBFNCS_SCOPE void fncs_update_time_delta(fncs_time delta);
+    FNCS_EXPORT void fncs_update_time_delta(fncs_time delta);
 
     /** Get the keys for all values that were updated during the last
      * time_request. */
-    LIBFNCS_SCOPE void fncs_get_events(char*** events, size_t *size);
+    FNCS_EXPORT void fncs_get_events(char*** events, size_t *size);
 
     /** Get a value from the cache with the given key.
      * Will hard fault if key is not found. */
-    LIBFNCS_SCOPE char * fncs_get_value(const char *key);
+    FNCS_EXPORT char * fncs_get_value(const char *key);
 
     /** Get an array of values from the cache with the given key.
      * Will return an array of size 1 if only a single value exists. */
-    LIBFNCS_SCOPE void fncs_get_values(
+    FNCS_EXPORT void fncs_get_values(
             const char *key,
             char *** values,
             size_t *size);
 
     /** Get an array of topic-value pairs from the cache with the given key.
      * Will return an array of size 1 if only a single value exists. */
-    LIBFNCS_SCOPE void fncs_get_matches(
+    FNCS_EXPORT void fncs_get_matches(
             const char *key,
             char *** topics,
             char *** values,
             size_t *size);
 
     /** Return the name of the simulator. */
-    LIBFNCS_SCOPE const char * fncs_get_name();
+    FNCS_EXPORT const char * fncs_get_name();
 
     /** Return a unique numeric ID for the simulator. */
-    LIBFNCS_SCOPE int fncs_get_id();
+    FNCS_EXPORT int fncs_get_id();
 
     /** Return the number of simulators connected to the broker. */
-    LIBFNCS_SCOPE int fncs_get_simulator_count();
+    FNCS_EXPORT int fncs_get_simulator_count();
 
 #ifdef __cplusplus
 }
--- src/fncs.hpp
+++ src/fncs.hpp
@@ -12,23 +12,16 @@ using ::std::pair;
 using ::std::string;
 using ::std::vector;
 
-#if defined _WIN32 && !defined __GNUC__
-# ifdef LIBFNCS_BUILD
-#  ifdef DLL_EXPORT
-#   define LIBFNCS_SCOPE            __declspec (dllexport)
-#   define LIBFNCS_SCOPE_VAR extern __declspec (dllexport)
-#  endif
-# elif defined _MSC_VER
-#  define LIBFNCS_SCOPE
-#  define LIBFNCS_SCOPE_VAR  extern __declspec (dllimport)
-# elif defined DLL_EXPORT
-#  define LIBFNCS_SCOPE             __declspec (dllimport)
-#  define LIBFNCS_SCOPE_VAR  extern __declspec (dllimport)
-# endif
-#endif
-#ifndef LIBFNCS_SCOPE
-# define LIBFNCS_SCOPE
-# define LIBFNCS_SCOPE_VAR extern
+#if defined (__WINDOWS__)
+#   if defined LIBFNCS_STATIC
+#       define FNCS_EXPORT
+#   elif defined LIBFNCS_EXPORTS
+#       define FNCS_EXPORT __declspec(dllexport)
+#   else
+#       define FNCS_EXPORT __declspec(dllimport)
+#   endif
+#else
+#   define FNCS_EXPORT
 #endif
 
 namespace fncs {
@@ -36,54 +29,54 @@ namespace fncs {
     typedef unsigned long long time;
 
     /** Connect to broker and parse config file. */
-    LIBFNCS_SCOPE void initialize();
+    FNCS_EXPORT void initialize();
 
     /** Connect to broker and parse inline configuration. */
-    LIBFNCS_SCOPE void initialize(const string &configuration);
+    FNCS_EXPORT void initialize(const string &configuration);
 
     /** Request the next time step to process. */
-    LIBFNCS_SCOPE time time_request(time next);
+    FNCS_EXPORT time time_request(time next);
 
     /** Publish value using the given key. */
-    LIBFNCS_SCOPE void publish(const string &key, const string &value);
+    FNCS_EXPORT void publish(const string &key, const string &value);
 
     /** Publish value using the given key, adding from:to into the key. */
-    LIBFNCS_SCOPE void route(const string &from, const string &to, const string &key, const string &value);
+    FNCS_EXPORT void route(const string &from, const string &to, const string &key, const string &value);
 
     /** Tell broker of a fatal client error. */
-    LIBFNCS_SCOPE void die();
+    FNCS_EXPORT void die();
 
     /** Close the connection to the broker. */
-    LIBFNCS_SCOPE void finalize();
+    FNCS_EXPORT void finalize();
 
     /** Update minimum time delta after connection to broker is made.
      * Assumes time unit is not changing. */
-    LIBFNCS_SCOPE void update_time_delta(time delta);
+    FNCS_EXPORT void update_time_delta(time delta);
 
     /** Get the keys for all values that were updated during the last
      * time_request. */
-    LIBFNCS_SCOPE vector<string> get_events();
+    FNCS_EXPORT vector<string> get_events();
 
     /** Get a value from the cache with the given key.
      * Will hard fault if key is not found. */
-    LIBFNCS_SCOPE string get_value(const string &key);
+    FNCS_EXPORT string get_value(const string &key);
 
     /** Get a vector of values from the cache with the given key.
      * Will return a vector of size 1 if only a single value exists. */
-    LIBFNCS_SCOPE vector<string> get_values(const string &key);
+    FNCS_EXPORT vector<string> get_values(const string &key);
 
     /** Get a vector of topic-value pairs from the cache with the given key.
      * Will return a vector of size 1 if only a single value exists. */
-    LIBFNCS_SCOPE vector<pair<string,string> > get_matches(const string &key);
+    FNCS_EXPORT vector<pair<string,string> > get_matches(const string &key);
 
     /** Return the name of the simulator. */
-    LIBFNCS_SCOPE string get_name();
+    FNCS_EXPORT string get_name();
 
     /** Return a unique numeric ID for the simulator. */
-    LIBFNCS_SCOPE int get_id();
+    FNCS_EXPORT int get_id();
 
     /** Return the number of simulators connected to the broker. */
-    LIBFNCS_SCOPE int get_simulator_count();
+    FNCS_EXPORT int get_simulator_count();
 
 }
 
