621bc20ad0c19ec2034f1241a4e3bf34439feae1
Jeff Daily <jeff.daily@pnnl.gov>
2015-09-01 11:16:27 -0700

    matlab build.sh script tests timestamps
    add fncs_publish mex

--- matlab/build.sh
+++ matlab/build.sh
@@ -24,8 +24,55 @@ echo "CPPFLAGS=$CPPFLAGS"
 echo " LDFLAGS=$LDFLAGS"
 echo "    LIBS=$LIBS"
 
+# what suffix does mex add?
+rm -f conftest.cpp
+cat << EOF > conftest.cpp
+#include "mex.h"
+void mexFunction( int nlhs, mxArray *plhs[],
+        int nrhs, const mxArray *prhs[] )
+{
+}
+EOF
+
+if $MEX conftest.cpp > /dev/null 2>&1
+then
+    echo "mex test file compiled successfully"
+    rm -f conftest.cpp
+else
+    echo "unable to compile test file"
+    rm -f conftest.*
+    exit 1
+fi
+
+EXT=
+for result in conftest.*
+do
+    EXT="${result##*.}"
+done
+
+if test "x$EXT" = x
+then
+    echo "failed to detect mex object extension"
+    exit 1
+fi
+echo "mex object extension is $EXT"
+
 for cpp in fncs_*.cpp
 do
-    echo "MEX $cpp"
-    $MEX $CPPFLAGS $LDFLAGS $LIBS $cpp || exit 1
+    do_mex=yes
+    base="${cpp%.*}"
+    if test -f $base.$EXT
+    then
+        if test $base.$EXT -nt $cpp
+        then
+            do_mex=no
+        fi
+    fi
+    if test "x$do_mex" = xyes
+    then
+        echo "MEX $cpp"
+        $MEX $CPPFLAGS $LDFLAGS $LIBS $cpp || exit 1
+    fi
 done
+
+echo "all files up to date"
