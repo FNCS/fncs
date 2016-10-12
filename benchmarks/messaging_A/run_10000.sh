266d31892f826f854441374619fc5c708967948a
Jeff Daily <jeff@node-6d-24.cybernet.lab>
2016-10-12 11:02:05 -0700

    update messaging_A benchmark for various sizes

--- benchmarks/messaging_A/run_10000.sh
+++ benchmarks/messaging_A/run_10000.sh
@@ -19,19 +19,19 @@ fi
 if test "x$FNCS_LOG_LEVEL" = xDEBUG4
 then
     BROKER_OUT=fncs_broker.out
-    T_OUT=t.out
+    T_OUT=t10000.out
 else
     BROKER_OUT=/dev/null
     T_OUT=/dev/null
 fi
 
-$ECHO "$BLD/fncs_broker > $BROKER_OUT 101 &"
-$BLD/fncs_broker > $BROKER_OUT 101 &
+$ECHO "$BLD/fncs_broker > $BROKER_OUT 10001 &"
+$BLD/fncs_broker > $BROKER_OUT 10001 &
 
-$ECHO "FNCS_CONFIG_FILE=t.zpl $BLD/benchmarks/bencher 1000 2 16 > $T_OUT &"
-FNCS_CONFIG_FILE=t.zpl $BLD/benchmarks/bencher 1000 2 16 > $T_OUT &
+$ECHO "FNCS_CONFIG_FILE=t10000.zpl $BLD/benchmarks/bencher 10000 2 16 > $T_OUT &"
+FNCS_CONFIG_FILE=t10000.zpl $BLD/benchmarks/bencher 10000 2 16 > $T_OUT &
 
-for i in `seq 0 99`
+for i in `seq 0 9999`
 do
     NAME=d$i
     if test "x$FNCS_LOG_LEVEL" = xDEBUG4
@@ -40,8 +40,8 @@ do
     else
         OUT=/dev/null
     fi
-    $ECHO "FNCS_CONFIG_FILE=d.zpl FNCS_NAME=$NAME $BLD/benchmarks/bencher 100 2 16 > $OUT &"
-    FNCS_CONFIG_FILE=d.zpl FNCS_NAME=$NAME $BLD/benchmarks/bencher 100 2 16 > $OUT &
+    $ECHO "FNCS_CONFIG_FILE=d.zpl FNCS_NAME=$NAME $BLD/benchmarks/bencher 1000 2 16 > $OUT &"
+    FNCS_CONFIG_FILE=d.zpl FNCS_NAME=$NAME $BLD/benchmarks/bencher 1000 2 16 > $OUT &
 done
 
 echo "launched all simulators, waiting..."
---
+++ benchmarks/messaging_A/run_5000.sh
@@ -0,0 +1,48 @@
+#!/bin/sh
+
+ECHO_OFF="${ECHO_OFF:-yes}"
+#export FNCS_LOG_LEVEL=DEBUG4
+
+if test "x$BLD" = x
+then
+    echo "must set BLD env var to top level build directory"
+    exit 1
+fi
+
+if test "x$ECHO_OFF" = xyes
+then
+    ECHO=:
+else
+    ECHO=echo
+fi
+
+if test "x$FNCS_LOG_LEVEL" = xDEBUG4
+then
+    BROKER_OUT=fncs_broker.out
+    T_OUT=t5000.out
+else
+    BROKER_OUT=/dev/null
+    T_OUT=/dev/null
+fi
+
+$ECHO "$BLD/fncs_broker > $BROKER_OUT 5001 &"
+$BLD/fncs_broker > $BROKER_OUT 5001 &
+
+$ECHO "FNCS_CONFIG_FILE=t5000.zpl $BLD/benchmarks/bencher 10000 2 16 > $T_OUT &"
+FNCS_CONFIG_FILE=t5000.zpl $BLD/benchmarks/bencher 10000 2 16 > $T_OUT &
+
+for i in `seq 0 4999`
+do
+    NAME=d$i
+    if test "x$FNCS_LOG_LEVEL" = xDEBUG4
+    then
+        OUT=$NAME.out
+    else
+        OUT=/dev/null
+    fi
+    $ECHO "FNCS_CONFIG_FILE=d.zpl FNCS_NAME=$NAME $BLD/benchmarks/bencher 1000 2 16 > $OUT &"
+    FNCS_CONFIG_FILE=d.zpl FNCS_NAME=$NAME $BLD/benchmarks/bencher 1000 2 16 > $OUT &
+done
+
+echo "launched all simulators, waiting..."
+wait
