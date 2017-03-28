515e9e5342dd3e30eb1e86ca530aa5aaddfccb38
Trevor Hardy <trevor.hardy@pnnl.gov>
2017-03-28 12:04:14 -0700

    Working first draft. Haven’t thoroughly tested anything but the simplest case and that works fine.

--- src/mini_federate.cpp
+++ src/mini_federate.cpp
@@ -34,6 +34,7 @@ Trevor Hardy
 #include "czmq.h"
 
 /* fncs headers */
+#include "log.hpp"
 #include "fncs.hpp"
 #include "fncs_internal.hpp"
 
@@ -61,10 +62,13 @@ Input arguments
     bool isRoot = FALSE;     // Setting a Boolean to make this frequent comparison operation faster
     string param_federate_type = "";
     string param_stop_time = "";
-    string param_update_interval = "";
-    string param_num_messages = "";
-    string param_message_size = "";
+    fncs::time param_update_interval = 0;
+    unsigned long param_num_messages = 0;
+    unsigned long param_message_size = 0;
     string value = "";
+    string key = "";
+    string real_key = "";
+    fncs::time target_time = 0;
     fncs::time time_granted = 0;
     fncs::time time_stop = 0;
     vector<string> events;
@@ -72,52 +76,59 @@ Input arguments
     ostream out(cout.rdbuf()); /* share cout's stream buffer */
 
     // Input parameter error-checking
-    // argc = number of input arguments + 1 (first parameter, argv[0], is mini_federate)
-    param_federate_type = argv[1];
-    param_stop_time = argv[2];
-    if param_federate_type.compare('root'){
-        isRoot = TRUE;
-        if (argc < 3) {
-            cerr << "Missing paramters" << endl;
-            cerr << "Usage: mini_federate root [simulation stop time]" << endl;
-            exit(EXIT_FAILURE);
-            }
-        else if (argc > 3) {
-            cerr << "Too many parameters." << endl;
-            cerr << "Usage: mini_federate root [simulation stop time]" << endl;
-            exit(EXIT_FAILURE);
-            }
-        else { //correct number of parameters
-            }
-    } else if param_federate_type.compare('leaf'){
-        isRoot = FALSE;
-        if (argc < 6) {
-            cerr << "Missing paramters" << endl;
-            cerr << "Usage: mini_federate leaf [simulation stop time]" <<\
-                    "[update interval] [messages/update] [message size]" << endl;
-            exit(EXIT_FAILURE);
-        }else if (argc > 6) {
-            cerr << "Too many parameters." << endl;
-            cerr << "Usage: mini_federate leaf [simulation stop time]" <<\
-                    "[update interval] [messages/update] [message size]" << endl;
-            exit(EXIT_FAILURE);
-        } else { //correct number of parameters
-            param_update_interval = atoul(argv[3].c_str());
-            param_num_messages = atoi(argv[4].c_str());
-            param_message_size = atoi(argv[5].c_str());
-        }
-    } else {
+    if (argc == 1){
         cerr << "First parameter must be 'leaf' or 'root'." << endl;
         exit(EXIT_FAILURE);
+    }else if (argc == 2){
+        cerr << "Second parameter must be simulation stop time." << endl;
+        exit(EXIT_FAILURE);
+    }else{
+        param_federate_type = argv[1];
+        param_stop_time = argv[2];
+        if (param_federate_type.compare("root") == 0){
+            isRoot = TRUE;
+            if (argc < 3) {
+                cerr << "Missing paramters" << endl;
+                cerr << "Usage: mini_federate root [simulation stop time]" << endl;
+                exit(EXIT_FAILURE);
+                }
+            else if (argc > 3) {
+                cerr << "Too many parameters." << endl;
+                cerr << "Usage: mini_federate root [simulation stop time]" << endl;
+                exit(EXIT_FAILURE);
+                }
+            else { //correct number of parameters
+                }
+        } else if (param_federate_type.compare("leaf") == 0){
+            isRoot = FALSE;
+            if (argc < 6) {
+                cerr << "Missing paramters" << endl;
+                cerr << "Usage: mini_federate leaf [simulation stop time]" <<\
+                        "[update interval] [messages/update] [message size]" << endl;
+                exit(EXIT_FAILURE);
+            }else if (argc > 6) {
+                cerr << "Too many parameters." << endl;
+                cerr << "Usage: mini_federate leaf [simulation stop time]" <<\
+                        "[update interval] [messages/update] [message size]" << endl;
+                exit(EXIT_FAILURE);
+            } else { //correct number of parameters
+                param_update_interval = atol(argv[3]);
+                param_num_messages = atol(argv[4]);
+                param_message_size = atol(argv[5]);
+            }
+        } else {
+            cerr << "First parameter must be 'leaf' or 'root'." << endl;
+            exit(EXIT_FAILURE);
+        }
     }
     
     // Creating message of appropriate size outside the main FNCS loop
     //  to save on computation during time-critial operation
     
     for (int idx = 0; idx < param_message_size; idx++){
-        value = value + "0";
+        value = value + "1";
     }
-    cout << "Standard message:  " << message << endl;
+    cout << "Standard topic value:  " << value << endl;
 
     // Connecting to FNCS broker
     fncs::initialize();
@@ -134,52 +145,64 @@ Input arguments
 
 
     // Starting co-sim
-    time_granted = 0; // at start of co-sim
+    LDEBUG2 << "Starting co-sim...";
+    target_time = param_update_interval; // at start of co-sim
     do {
         
         // Making time request
         fncs::time event;
-        if isRoot{
+        if (isRoot) {
             // Always requests stop time of simulation since root only echos back
             //  packets; no endogenous activity
             time_granted = fncs::time_request(time_stop);
             }
         else{
             // Request one update_interval from the last granted time.
-            requesting_time = fncs::time(param_update_interval) + time_granted;
-            time_granted = fncs::time_request(requesting_time);
+            time_granted = fncs::time_request(target_time);
+            LDEBUG4 << "Requesting time: " << target_time;
+            LDEBUG4 << "Granted time: " << time_granted;
             }
-        cout << "time_granted is " << time_granted << endl;
         
         // "Processing" received events at granted time
         events = fncs::get_events();
+        LDEBUG3 << "Getting events...";
+        LDEBUG4 << "Number of events: " << events.size();
         for (vector<string>::iterator it=events.begin(); it!=events.end(); ++it) {
             key = *it;
             value = fncs::get_value(*it);
             out << time_granted
-                << "\t" << *it
+                << "\t" << key
                 << "\t" << value
                 << endl;
-            if isRoot{
+            size_t pos = key.find('/');
+            real_key = key.substr(pos+1,string::npos);
+            LDEBUG4 << "key: " << key;
+            LDEBUG4 << "real_key: " << real_key;
+            if (isRoot) {
                 // Echoing back received message
-                fncs::publish(key, value);
+                fncs::publish(real_key, value);
+                LDEBUG4 << "Publishing message with key, value: " << real_key << "," << value;
             }
             else{
                 // Leaf does nothing with received messages
             }
-           
+        }
+        if (time_granted == target_time){
+            LDEBUG3 << "Publishing FNCS messages...";
             // Generating new messages at granted time
-            if isRoot{
+            if (isRoot) {
                 // Root node never generates messages on its own, only echos back
                 //  what has been received.
             }
             else{
-                for(int idx = 0; idx < param_num_messages; idx++ ){
+                for(int idx = 1; idx <= param_num_messages; idx++ ){
                     string key = "key" + to_string(idx);
                     fncs::publish(key, value);
                 }
             }
+        target_time = target_time + param_update_interval;
         }
+        
     } while (time_granted < time_stop);
     cout << "time_granted was " << time_granted << endl;
     cout << "time_stop was " << time_stop << endl;
