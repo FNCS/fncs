7d9053573ddad76eb6f63fe26e3c1a1977774772
Hardy, Trevor D <trevor.hardy@pnnl.gov>
2017-05-16 14:29:53 -0700

    Add LERROR output when unrecognized message type received

--- src/fncs.cpp
+++ src/fncs.cpp
@@ -752,7 +752,7 @@ fncs::time fncs::time_request(fncs::time time_next)
                 }
             }
             else {
-                LERROR << "unrecognized message type";
+                LERROR << "unrecognized message type: " << message_type;
                 die();
                 return time_next;
             }
