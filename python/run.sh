cf47442da449016c4a3094e5f352c1b9df3cf234
temcdrm <Thomas.McDermott@pnnl.gov>
2017-08-07 09:08:59 -0700

    use the correct yaml file

--- python/run.sh
+++ python/run.sh
@@ -1,5 +1,5 @@
-(export FNCS_LOG_STDOUT=yes && export FNCS_LOG_LEVEL=DEBUG4 && exec fncs_broker 2 &> broker.log &)
+(export FNCS_LOG_STDOUT=yes && export FNCS_LOG_LEVEL=DEBUG4 && export FNCS_TRACE=yes && exec fncs_broker 2 &> broker.log &)
 (export FNCS_LOG_STDOUT=yes && export FNCS_LOG_LEVEL=DEBUG4 && exec python test.py &> test.log &)
-(export FNCS_LOG_STDOUT=yes && export FNCS_LOG_LEVEL=DEBUG4 && export FNCS_CONFIG_FILE=tracer.yaml && exec python pytracer.py 100 pytracer.out &> pytracer.log &)
+(export FNCS_LOG_STDOUT=yes && export FNCS_LOG_LEVEL=DEBUG4 && export FNCS_CONFIG_FILE=pytracer.yaml && exec python pytracer.py 100 pytracer.out &> pytracer.log &)
 # (export FNCS_LOG_STDOUT=yes && export FNCS_LOG_LEVEL=DEBUG4 && export FNCS_CONFIG_FILE=tracer.yaml && exec fncs_tracer 100s tracer.out &> tracer.log &)
 
